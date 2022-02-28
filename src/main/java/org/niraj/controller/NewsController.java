abeaee251fcc1c600854e29d5df5a1b0cfd43bc3
Niraj <niraj.webtech@gmail.com>
2022-03-01 03:51:58 +0530

    Adding for loop

--- src/main/java/org/niraj/controller/NewsController.java
+++ src/main/java/org/niraj/controller/NewsController.java
@@ -11,6 +11,10 @@ public class NewsController {
 
 	@RequestMapping(value = "/welcome" , method = RequestMethod.GET)
 	public String getNews() {
+		
+		for (int i = 0 ; i<5 ;i++ ) {
+			System.out.println("News Publication calling => " + i);
+		}
 		return "welcome to Bihar news API call";
 	}
 		
