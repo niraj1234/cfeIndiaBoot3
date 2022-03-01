cc17b248528630db7aaae9cfd9a0ce9e7553a480
Niraj <niraj.webtech@gmail.com>
2022-03-01 15:25:49 +0530

    Adding random site address for NewsController

--- src/main/java/org/niraj/controller/NewsController.java
+++ src/main/java/org/niraj/controller/NewsController.java
@@ -1,6 +1,7 @@
 package org.niraj.controller;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,17 +15,24 @@ public class NewsController {
 	@RequestMapping(value = "/welcome" , method = RequestMethod.GET)
 	public String getNews() {
 		
-		for (int i = 0 ; i<5 ;i++ ) {
-			System.out.println("News Publication calling => " + i);
-		}
+//		for (int i = 0 ; i<2 ;i++ ) {
+//			System.out.println("News Publication calling => " + i);
+//		}
 		
 		ArrayList publicationList = new ArrayList<String>();
 		publicationList.add("www.patnaInfo.com");
 		publicationList.add("www.hindustanTimes.com");
 		publicationList.add("www.khojo.online");
 		publicationList.add("www.newsdekho.in");
-				
-		return "welcome to Bihar news API call";
+		publicationList.add("www.kauwa.online");
+		publicationList.add("www.work1.com");
+		
+		
+		Random rand = new Random();
+		int upperlimit = 5;
+		int randNum = rand.nextInt(upperlimit);		
+		String siteName = (String) publicationList.get(randNum);
+		return siteName;
 	}
 		
 }
